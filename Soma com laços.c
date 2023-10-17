eef84682f5a015c8bab5f1e76efdbc951344d5c2
Roger Luís <137456008+RogerLSantana@users.noreply.github.com>
2023-10-17 16:13:33 -0300

    Add files via upload

---
+++ Soma com laços.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+int main()
+{
+    int i, numero, soma;
+
+    setlocale(LC_ALL, "portuguese");
+
+    soma = 0;
+
+    for (i = 1; i <= 5; i++)
+    {
+        printf("Digite %dº número:  \n", i);
+        scanf("%d", &numero);
+        soma = numero + soma;
+    }
+
+    printf("Soma de todos os número: %d", soma);
+
+    getchar();
+    return 0;
+}
