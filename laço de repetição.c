d6ef57a7a98c97cbb20834056a734fed15b42cd6
Roger Luís <137456008+RogerLSantana@users.noreply.github.com>
2023-10-17 15:44:09 -0300

    Add files via upload

---
+++ laço de repetição.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "protuguese");
+
+    int i, numero;
+
+    printf("digite um numero de 1 até 10: \n");
+    scanf("%d", &numero);
+
+    /* de 1 até 10
+       para i de i até 10 passo 1 faça
+       i=i+2
+       i+=2
+    */
+
+    setlocale(LC_ALL, "portuguese");
+
+    printf("Tabuada do %d\n", numero);
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d x %d = %d \n", numero, i, numero * i);
+    }
+
+    getchar();
+    return 0;
+}
