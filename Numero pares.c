7ca933b7c3152cd2ed0c229ede588764e5864912
Roger Luís <137456008+RogerLSantana@users.noreply.github.com>
2023-10-17 15:44:28 -0300

    Add files via upload

---
+++ Numero pares.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+int main()
+{
+    int i;
+    setlocale(LC_ALL, "portuguese");
+
+    for (i = 2; i <= 20; i++)
+    {
+        if (i % 2 == 0)
+        {
+            printf("%d \n", i);
+        }
+    }
+
+    getchar();
+    return 0;
+}
